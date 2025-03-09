3d1631304c30d2982b295bff13c0398604fbe652
bibibobobi <smilingcat33@gmail.com>
2025-03-10 01:25:42 +0800

    fix: build error

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,18 +1,2 @@
-import { NextRequest } from "next/server";
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(
-  request: NextRequest,
-  context: { params: { kindeAuth: string } }
-) {
-  const { kindeAuth } = context.params;
-  return handleAuth(request, kindeAuth);
-}
-
-export async function POST(
-  request: NextRequest,
-  context: { params: { kindeAuth: string } }
-) {
-  const { kindeAuth } = context.params;
-  return handleAuth(request, kindeAuth);
-}
+export const GET = handleAuth();

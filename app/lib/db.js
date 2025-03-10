c3bea60f4079298d9a17e448f248b57a77b315d8
bibibobobi <smilingcat33@gmail.com>
2025-03-10 17:25:30 +0800

    fix: build error

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,9 +2,9 @@
 import { PrismaClient } from "@prisma/client";
 
 // Prevent multiple instances of Prisma Client in development
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
 export const prisma = globalThis.prisma || new PrismaClient();
 
